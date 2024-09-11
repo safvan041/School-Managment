9bfcbf5f97c529c2f93c654661d25627b2ee6e05
BK <safvanbakkar041@gmail.com>
2024-09-11 13:26:34 +0530

    Db issue overcome

--- attendance_page.py
+++ attendance_page.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 # Setup MongoDB connection
 client = MongoClient('mongodb://localhost:27017/')
-db = client['school_management']
+db = client.school
 attendance = db['attendance']
 
 class AttendancePage(Frame):
